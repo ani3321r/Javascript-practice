637c91ad54697ddc504720770f3affdd9743c495
Aniruddha Roy <65581823+ani3321r@users.noreply.github.com>
2024-01-14 17:18:48 +0000

    classes, inheritence and static prop

---
+++ classes_and_oop/inheritence.js
@@ -0,0 +1,30 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+}
+
+class Teacher extends User{
+    constructor(username, email, password){
+        super(username)//we don't have to use "this",the way we did in call anymore(syntactic sugar)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.logMe()
+const masalaChai = new User("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof User);//kind of we can say chai is sub class of User
