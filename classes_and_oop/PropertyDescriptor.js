e2289cfbd7be651eb123f51d9ef45c3959051a67
Aniruddha Roy <65581823+ani3321r@users.noreply.github.com>
2024-01-15 14:53:34 +0000

    property descriptor

---
+++ classes_and_oop/PropertyDescriptor.js
@@ -0,0 +1,34 @@
+const descripter = Object.getOwnPropertyDescriptor(Math, "PI")
+
+// console.log(descripter);
+
+// console.log(Math.PI);
+// Math.PI = 5
+// console.log(Math.PI);
+
+const chai = {
+    name: 'ginger chai',
+    price: 250,
+    isAvailable: true,
+
+    orderChai: function(){
+        console.log("chai nhi bni");
+    }
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+Object.defineProperty(chai, 'name', {
+    //writable: false,
+    enumerable: true,
+    
+})
+
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+for (let [key, value] of Object.entries(chai)) {//object is not by default iteratable we must call by object.entries
+    if (typeof value !== 'function') {//if we include funtion code faat jaega
+        
+        console.log(`${key} : ${value}`);
+    }
+}
